9065a32cddafce165095d966bd7c4d3a6881f32e
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-23 11:48:24 -0500

    Update predict.js

--- src/predict.js
+++ src/predict.js
@@ -17,14 +17,14 @@ async function init(base) {
     const input = tf.node.decodeImage(buf);
     var response = "";
 
-    pred = model.predict(input.expandDims(0).resizeNearestNeighbor([256,256]).toFloat())
+    pred = model.predict(input.expandDims(0).resizeNearestNeighbor([256,256]).toFloat());
 
-    pred.print()
-    console.log("End of predict function")
+    pred.print();
+    console.log("End of predict function");
     //This array is encoded with index i = corresponding emotion. In dataset, 0 = Angry, 1 = Disgust, 2 = Fear, 3 = Happy, 4 = Sad, 5 = Surprise and 6 = Neutral
-    category = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+    category = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash'];
     //At which index in tensor we get the largest value ?
-    
+    /* console.log("DOS");
     pred.data()
         .then((data) => {
             console.log(data)
@@ -38,9 +38,20 @@ async function init(base) {
             }
             response += category[max_val_index]
             return response
-        })
+        });
+ */
 
-    
+        console.log(pred.arraySync()[1]);
+        /* for (let i = 0; i < pred.data().length; i++) {
+            max_val = -1
+            max_val_index = -1
+            for (let i = 0; i < data.length; i++) {
+                if (data[i] > max_val) {
+                    max_val = data[i]
+                    max_val_index = i
+                }
+            }
+          } */
         
 }
 
